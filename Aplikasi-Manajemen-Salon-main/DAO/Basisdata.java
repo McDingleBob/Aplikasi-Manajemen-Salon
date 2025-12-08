c911513267380f8ad9386182480212d94b2e549a
JundanAbdulWahhab <lckybrothersi.iiyt@gmail.com>
2025-12-08 16:39:24 +0700

    Mengubah nama ke Indonesia

--- Aplikasi-Manajemen-Salon-main/DAO/Basisdata.java
+++ Aplikasi-Manajemen-Salon-main/DAO/Basisdata.java
@@ -4,7 +4,7 @@ import java.sql.Connection;
 import java.sql.DriverManager;
 import java.sql.SQLException;
 
-public class Database {
+public class Basisdata {
 
     public static final String URL = "jdbc:mysql://localhost:3306/database_salon";
     public static final String USER = "root";
---
+++ Aplikasi-Manajemen-Salon-main/Entitas/Transaksi.java
@@ -0,0 +1,9 @@
+package Entitas;
+
+import java.util.Date;
+
+public class Transaksi {
+    private String idTransaksi;
+    private Date tanggal;
+    private
+}
